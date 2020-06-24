aeddaaf8732c79b465d91e882c9c014f1ac7bb0e
Keriann <keriann.lin@gmail.com>
2020-06-24 16:17:10 -0400

    added react router

--- client/App.jsx
+++ client/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import LoginContainer from './containers/LoginContainer.jsx';
 import MainContainer from './containers/MainContainer.jsx';
 import './stylesheets/styles.scss';
 
+
 function App() {
   return (
-    <>
-      <MainContainer />
-    </>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/">
+            <LoginContainer />
+          </Route>
+          <Route path="/game">
+            <MainContainer />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
